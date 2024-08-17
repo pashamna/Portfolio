82f9dd115990d8a36afa55073a3f7c01b5f1918a
thughari <thughari3@gmail.com>
2024-08-17 18:51:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});

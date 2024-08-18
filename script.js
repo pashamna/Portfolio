cc2bced98e39cb7b4048d555c53b2c598ce8ef08
thughari <thughari3@gmail.com>
2024-08-18 15:06:39 +0530

    template done

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-});
